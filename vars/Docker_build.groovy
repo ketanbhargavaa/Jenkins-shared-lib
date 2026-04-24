a0477f978b20ab3ddaba46b44f53b33442a35215
ketanbhargavaa <ketanbhargava0206@gmail.com>
2026-04-24 16:25:07 +0530

    Enhance Docker build script with progress messages
    Added echo statements to indicate build progress.

--- vars/Docker_build.groovy
+++ vars/Docker_build.groovy
@@ -1,3 +1,5 @@
 def cal(String projectName, String ImageTag, String DockerHubUser){
+  echo 'this is building the code'
   sh "docker build -t ${DockerHubUser}/${projectName}:${latest} ."
+  echo 'image build successfully'
 }
